e834a032f177497e61902c7424420d0412033460
Devin <devhunter16@gmail.com>
2023-06-23 16:57:18 -0700

    Imported Provider components and passed in our data store as a prop in order for our app (and any child components within) to be able to tap into the data within the store.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// Importing the Provider component from react-redux so that we can wrap our app in it.
+// We are doing this so we can provide our data store by passing it in as a prop.
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
+// Importing our store from the file in which we created it so that we can provide it to
+// our app. We can then pass our store as a prop to the Provider component so that our
+// whole app (and any child components within) can tap into that store in order to: get
+// data from the store, set up a subscription to any store data updates, or dispatch
+// actions in order to change the data within the store.
+import store from './store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Provider store={store}><App /></Provider>);
